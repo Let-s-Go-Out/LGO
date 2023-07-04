f8d9cbfa764fae7728c1837c0d7fefcf014ffe97
salmon99 <namhj99@naver.com>
2023-07-04 16:38:59 +0900

    7/4 '출발지를 입력하세요' 칸을 검색 가능한 지도가 나오도록 바꾸려고 찾아보는 중...

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:developer' show log;
 import 'package:flutter/material.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart' as NaverMap;
 
@@ -147,4 +149,50 @@ class _MainPageState extends State<MainPage> {
       child: Text(concept),
     );
   }
+}
+
+class TestPage extends StatefulWidget {
+  const TestPage({Key? key}) : super(key: key);
+
+  @override
+  State<TestPage> createState() => TestPageState();
+}
+
+class TestPageState extends State<TestPage> {
+  late NaverMapController _mapController;
+  final Completer<NaverMapController> mapControllerCompleter = Completer();
+
+  @override
+  Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context);
+    final pixelRatio = mediaQuery.devicePixelRatio;
+    final mapSize =
+    Size(mediaQuery.size.width - 32, mediaQuery.size.height - 72);
+    final physicalSize =
+    Size(mapSize.width * pixelRatio, mapSize.height * pixelRatio);
+
+    print("physicalSize: $physicalSize");
+
+    return Scaffold(
+      backgroundColor: const Color(0xFF343945),
+      body: Center(
+          child: SizedBox(
+              width: mapSize.width,
+              height: mapSize.height,
+              // color: Colors.greenAccent,
+              child: _naverMapSection())),
+    );
+  }
+
+  Widget _naverMapSection() => NaverMap(
+    options: const NaverMapViewOptions(
+        indoorEnable: true,
+        locationButtonEnable: false,
+        consumeSymbolTapEvents: false),
+    onMapReady: (controller) async {
+      _mapController = controller;
+      mapControllerCompleter.complete(controller);
+      log("onMapReady", name: "onMapReady");
+    },
+  );
 }
