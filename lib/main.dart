72f0953f517a326e0c108341385b783f87d2315d
Hyeon Jung Nam <namhj99@naver.com>
2023-07-12 17:00:34 +0900

    7/12 구글 맵 내위치

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,59 @@
-// import 'dart:developer';
-// import 'package:flutter/material.dart';
-// import 'package:geolocator/geolocator.dart';
-// import 'package:nagaja_app/main_page.dart';
-// import 'package:permission_handler/permission_handler.dart';
-//
-// void main() async {
-//   await _initialize();
-//   _permission();
-//   runApp(MyApp());
-// }
-//
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Outing Routes App',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: MainPage(), // Set the main page as the home screen
-//     );
-//   }
-// }
-//
-// Future<void> _initialize() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-// }
-//
-// void _permission() async {
-//   var requestStatus = await Permission.location.request();
-//   var status = await Permission.location.status;
-//   if (requestStatus.isGranted && status.isLimited) {
-//     // isLimited - 제한적 동의 (ios 14 < )
-//     // 요청 동의됨
-//     print("isGranted");
-//     if (await Permission.locationWhenInUse.serviceStatus.isEnabled) {
-//       // 요청 동의 + gps 켜짐
-//       var position = await Geolocator.getCurrentPosition();
-//       print("serviceStatusIsEnabled position = ${position.toString()}");
-//     } else {
-//       // 요청 동의 + gps 꺼짐
-//       print("serviceStatusIsDisabled");
-//     }
-//   } else if (requestStatus.isPermanentlyDenied || status.isPermanentlyDenied) {
-//     // 권한 요청 거부, 해당 권한에 대한 요청에 대해 다시 묻지 않음 선택하여 설정화면에서 변경해야함. android
-//     print("isPermanentlyDenied");
-//     openAppSettings();
-//   } else if (status.isRestricted) {
-//     // 권한 요청 거부, 해당 권한에 대한 요청을 표시하지 않도록 선택하여 설정화면에서 변경해야함. ios
-//     print("isRestricted");
-//     openAppSettings();
-//   } else if (status.isDenied) {
-//     // 권한 요청 거절
-//     print("isDenied");
-//   }
-//   print("requestStatus ${requestStatus.name}");
-//   print("status ${status.name}");
-// }
-//
+import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:nagaja_app/main_page.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-void main() => runApp(const MyApp());
-
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
+void main() async {
+  await _initialize();
+  _permission();
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Maps Sample App'),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
+      title: 'Outing Routes App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MainPage(), // Set the main page as the home screen
     );
   }
+}
+
+Future<void> _initialize() async {
+  WidgetsFlutterBinding.ensureInitialized();
+}
+
+void _permission() async {
+  var requestStatus = await Permission.location.request();
+  var status = await Permission.location.status;
+  if (requestStatus.isGranted && status.isLimited) {
+    // isLimited - 제한적 동의 (ios 14 < )
+    // 요청 동의됨
+    print("isGranted");
+    if (await Permission.locationWhenInUse.serviceStatus.isEnabled) {
+      // 요청 동의 + gps 켜짐
+      var position = await Geolocator.getCurrentPosition();
+      print("serviceStatusIsEnabled position = ${position.toString()}");
+    } else {
+      // 요청 동의 + gps 꺼짐
+      print("serviceStatusIsDisabled");
+    }
+  } else if (requestStatus.isPermanentlyDenied || status.isPermanentlyDenied) {
+    // 권한 요청 거부, 해당 권한에 대한 요청에 대해 다시 묻지 않음 선택하여 설정화면에서 변경해야함. android
+    print("isPermanentlyDenied");
+    openAppSettings();
+  } else if (status.isRestricted) {
+    // 권한 요청 거부, 해당 권한에 대한 요청을 표시하지 않도록 선택하여 설정화면에서 변경해야함. ios
+    print("isRestricted");
+    openAppSettings();
+  } else if (status.isDenied) {
+    // 권한 요청 거절
+    print("isDenied");
+  }
+  print("requestStatus ${requestStatus.name}");
+  print("status ${status.name}");
 }
--- lib/map_browse_screen.dart
+++ lib/map_browse_screen.dart
@@ -1,70 +1,110 @@
-// import 'package:flutter/material.dart';
-// import 'package:geolocator/geolocator.dart';
-// import 'package:flutter/foundation.dart';
-//
-// class MapBrowseScreen extends StatefulWidget {
-//   const MapBrowseScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   _MapBrowseScreenState createState() => _MapBrowseScreenState();
-// }
-//
-// class _MapBrowseScreenState extends State<MapBrowseScreen> {
-//   late Position _position;
-//
-//   get controller => null;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _getPosition();
-//   }
-//
-//   @override
-//   void dispose() {
-//     super.dispose();
-//   }
-//
-//   void _getPosition() async{
-//     try {
-//       LocationPermission permission = await Geolocator.requestPermission();
-//       Position position = await Geolocator.getCurrentPosition(
-//           desiredAccuracy: LocationAccuracy.high);
-//       setState(() {
-//         _position = position; // 위치 정보 업데이트
-//       });
-//       print(position);
-//     }catch(e){
-//       print('There was a problem with the internet connection.');
-//     }
-//   }
-//
-//   @override
-//   Future<void> _performSearch() async {
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('지도 검색'),
-//       ),
-//       body: Column(children: [
-//         Container(
-//           padding: EdgeInsets.all(8.0),
-//           child: TextField(
-//             controller: controller,
-//             decoration: InputDecoration(
-//               hintText: '장소를 검색하세요.',
-//               suffixIcon: IconButton(
-//                 icon: Icon(Icons.search),
-//                 onPressed: _performSearch,
-//               ),
-//             ),
-//             onSubmitted: (_) => _performSearch(),
-//           ),
-//         ),
-//       ]),
-//     );
-//   }
-// }
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:flutter/foundation.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart';
+
+class MapBrowseScreen extends StatefulWidget {
+  const MapBrowseScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MapBrowseScreen> createState() => _MapBrowseScreenState();
+}
+
+class _MapBrowseScreenState extends State<MapBrowseScreen> {
+  late GoogleMapController mapController;
+  late TextEditingController textController;
+  final LatLng _center = const LatLng(127.0203333, 37.58638333);
+  Position? _position;
+
+  get address async => await myAddress();
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    textController = TextEditingController();
+    initText();
+    _getPosition();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future<void> initText() async {
+    var address = await myAddress();
+  }
+
+  Future<Position> _getPosition() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _position = position; // 위치 정보 업데이트
+    });
+    print(position);
+    return position;
+  }
+
+  Future<String> myAddress() async {
+    var gps = await _getPosition();
+    // 좌표로 주소 구하기
+    String lat = gps.latitude.toString();
+    String lon = gps.longitude.toString();
+    Response response = await get(Uri.parse('https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lon&key=AIzaSyAIeZMzg3xE5dYXgiWNoIjDE34R0SzTAzE'));
+
+    String jsonData = response.body;
+    return jsonDecode(jsonData)['results'][0]['formatted_address'];
+  }
+
+  Future<void> _performSearch() async {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('지도 검색'),
+      ),
+      body: Column(children: [
+        Container(
+          padding: const EdgeInsets.all(8.0),
+          child: TextField(
+            controller: textController,
+            decoration: InputDecoration(
+              hintText: '장소를 검색하세요.',
+              labelText: address,
+              suffixIcon: IconButton(
+                icon: const Icon(Icons.search),
+                onPressed: _performSearch,
+              ),
+            ),
+            onSubmitted: (_) => _performSearch(),
+          ),
+        ),
+        Expanded(
+            child: GoogleMap(
+              myLocationEnabled: true,
+          myLocationButtonEnabled: false,
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 15.0,
+          ),
+        )),
+        FloatingActionButton(onPressed: () async {
+          var gps = await _getPosition();
+          mapController.animateCamera(CameraUpdate.newLatLng(LatLng(gps.latitude, gps.longitude)));
+          },backgroundColor: Colors.white,
+        child: const Icon(Icons.my_location, color: Colors.black,),),
+      ]),
+    );
+  }
+}
