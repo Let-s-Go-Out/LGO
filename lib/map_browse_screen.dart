8c011bcf193632baf6df5d78cc7e5e3e4f8b5f9a
salmon99 <namhj99@naver.com>
2023-07-13 01:12:20 +0900

    7/12 현재 위치 좌표 -> 주소로 변환하고 검색창에 출력되도록 함. 검색창에 검색하면 검색 결과가 나오도록 함. 검색 결과 클릭한 후 이벤트 구현해야 함.

--- lib/map_browse_screen.dart
+++ lib/map_browse_screen.dart
@@ -1,8 +1,6 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:flutter/foundation.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart';
 
@@ -16,10 +14,11 @@ class MapBrowseScreen extends StatefulWidget {
 class _MapBrowseScreenState extends State<MapBrowseScreen> {
   late GoogleMapController mapController;
   late TextEditingController textController;
-  final LatLng _center = const LatLng(127.0203333, 37.58638333);
+  final LatLng _center = const LatLng(37.58638333, 127.0203333);
   Position? _position;
-
-  get address async => await myAddress();
+  String? address;
+  List<dynamic> searchResults = [];
+  bool showSearchResults = false;
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -28,10 +27,23 @@ class _MapBrowseScreenState extends State<MapBrowseScreen> {
   @override
   void initState() {
     super.initState();
-
     textController = TextEditingController();
-    initText();
-    _getPosition();
+    _getPosition().then((position) {
+      setState(() {
+        _position = position;
+        _updateCameraPosition();
+      });
+      _getMyAddress().then((myAddress) {
+        setState(() {
+          address = myAddress;
+          textController.text = address ?? '';
+        });
+      }).catchError((error) {
+        print('Error retrieving address: $error');
+      });
+    }).catchError((error) {
+      print('Error retrieving position: $error');
+    });
   }
 
   @override
@@ -40,32 +52,86 @@ class _MapBrowseScreenState extends State<MapBrowseScreen> {
   }
 
   Future<void> initText() async {
-    var address = await myAddress();
+    String? myAddress = await _getMyAddress();
+    setState(() {
+      address = myAddress;
+      textController.text = address ?? '';
+    });
   }
 
   Future<Position> _getPosition() async {
     LocationPermission permission = await Geolocator.requestPermission();
     Position position = await Geolocator.getCurrentPosition(
         desiredAccuracy: LocationAccuracy.high);
-    setState(() {
-      _position = position; // 위치 정보 업데이트
-    });
-    print(position);
     return position;
   }
 
-  Future<String> myAddress() async {
-    var gps = await _getPosition();
-    // 좌표로 주소 구하기
-    String lat = gps.latitude.toString();
-    String lon = gps.longitude.toString();
-    Response response = await get(Uri.parse('https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lon&key=AIzaSyAIeZMzg3xE5dYXgiWNoIjDE34R0SzTAzE'));
-
-    String jsonData = response.body;
-    return jsonDecode(jsonData)['results'][0]['formatted_address'];
+  Future<String?> _getMyAddress() async {
+    if (_position != null) {
+      String lat = _position!.latitude.toString();
+      String lon = _position!.longitude.toString();
+      Response response = await get(Uri.parse(
+          'https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyAIeZMzg3xE5dYXgiWNoIjDE34R0SzTAzE&language=ko&latlng=${lat},${lon}'));
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = jsonDecode(response.body);
+        if (data['results'] != null && data['results'].isNotEmpty) {
+          String? myAddress =
+              data['results'][0]['formatted_address'] as String?;
+          print("myAddress: $myAddress");
+          return myAddress;
+        }
+      }
+    }
+    return null;
   }
 
-  Future<void> _performSearch() async {}
+  Future<void> searchPlaces(String keyword) async {
+    String apiKey = 'AIzaSyAIeZMzg3xE5dYXgiWNoIjDE34R0SzTAzE';
+    String baseUrl =
+        'https://maps.googleapis.com/maps/api/place/textsearch/json';
+
+    // 검색 요청 URL 생성
+    String url = '$baseUrl?key=$apiKey&language=ko&query=$keyword';
+
+    // HTTP 요청 보내기
+    Response response = await get(Uri.parse(url));
+
+    // 결과 파싱
+    if (response.statusCode == 200) {
+      dynamic jsonData = jsonDecode(response.body);
+      List<dynamic> results = jsonData['results'];
+      setState(() {
+        searchResults = results;
+        showSearchResults = true;
+      });
+    } else {
+      throw Exception('Failed to search places');
+    }
+  }
+
+  Future<void> _performSearch() async {
+    String searchText = textController.text.trim();
+    if (searchText.isNotEmpty) {
+      setState(() {
+        showSearchResults = true;
+      });
+    }
+  }
+
+  void _clearSearchResults() {
+    setState(() {
+      showSearchResults = false;
+      searchResults.clear();
+    });
+  }
+
+  void _updateCameraPosition() {
+    if (_position != null) {
+      mapController.animateCamera(CameraUpdate.newLatLng(
+        LatLng(_position!.latitude, _position!.longitude),
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,33 +143,78 @@ class _MapBrowseScreenState extends State<MapBrowseScreen> {
         Container(
           padding: const EdgeInsets.all(8.0),
           child: TextField(
-            controller: textController,
-            decoration: InputDecoration(
-              hintText: '장소를 검색하세요.',
-              labelText: address,
-              suffixIcon: IconButton(
-                icon: const Icon(Icons.search),
-                onPressed: _performSearch,
+              controller: textController,
+              decoration: InputDecoration(
+                hintText: '장소를 검색하세요.',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    String searchText = textController.text.trim();
+                    if (searchText.isNotEmpty) {
+                      searchPlaces(searchText);
+                    }
+                  },
+                ),
               ),
-            ),
-            onSubmitted: (_) => _performSearch(),
-          ),
+              onSubmitted: (_) {
+                String searchText = textController.text.trim();
+                if (searchText.isNotEmpty) {
+                  searchPlaces(searchText);
+                }
+              }),
         ),
-        Expanded(
-            child: GoogleMap(
-              myLocationEnabled: true,
-          myLocationButtonEnabled: false,
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 15.0,
+        if (showSearchResults && searchResults.isNotEmpty)
+          Expanded(
+            child: ListView.builder(
+              itemCount: searchResults.length,
+              itemBuilder: (context, index) {
+                dynamic result = searchResults[index];
+                String name = result['name'];
+                String address = result['formatted_address'];
+                return ListTile(
+                  title: Text(name),
+                  subtitle: Text(address),
+                  onTap: () {},
+                );
+              },
+            ),
           ),
-        )),
-        FloatingActionButton(onPressed: () async {
-          var gps = await _getPosition();
-          mapController.animateCamera(CameraUpdate.newLatLng(LatLng(gps.latitude, gps.longitude)));
-          },backgroundColor: Colors.white,
-        child: const Icon(Icons.my_location, color: Colors.black,),),
+        if (!showSearchResults || searchResults.isEmpty)
+          Expanded(
+            child: Stack(children: [
+              GoogleMap(
+                myLocationEnabled: true,
+                myLocationButtonEnabled: false,
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 15.0,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0, bottom: 10.0),
+                  child: FloatingActionButton(
+                    onPressed: () async {
+                      if (showSearchResults) {
+                        _clearSearchResults();
+                      } else {
+                        var gps = await _getPosition();
+                        mapController.animateCamera(CameraUpdate.newLatLng(
+                            LatLng(_position!.latitude, _position!.longitude)));
+                      }
+                    },
+                    backgroundColor: Colors.white,
+                    child: const Icon(
+                      Icons.my_location,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+            ]),
+          ),
       ]),
     );
   }
