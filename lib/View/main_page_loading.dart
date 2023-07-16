bca5442f8e3dda17c4ff1e94a246ae3948ffa9fe
salmon99 <namhj99@naver.com>
2023-07-16 13:26:07 +0900

    7/16 임시 페이지

--- lib/View/main_page_loading.dart
+++ lib/View/main_page_loading.dart
@@ -1,19 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:nagaja_app/View/main_route_page.dart';
 
 class MainLoadingPage extends StatelessWidget {
-  const MainLoadingPage({super.key});
+  const MainLoadingPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: Text(
-            'AI가 나들이 코스를 계획하고 있어요',
-            style: TextStyle(fontSize: 20, color:Colors.black),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'AI is planning an outing course',
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            ),
+            SizedBox(height: 16), // Add some space between the text and the button
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MainRoutePage()),
+                );
+              },
+              child: Text('경로안내 페이지로 이동'),
+            ),
+          ],
         ),
-        // loading 완료 후
-
       ),
     );
   }
---
+++ lib/View/main_route_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MainRoutePage extends StatefulWidget {
+  const MainRoutePage({Key? key}) : super(key: key);
+
+  @override
+  _MainRoutePageState createState() => _MainRoutePageState();
+}
+
+class _MainRoutePageState extends State<MainRoutePage> {
+  late GoogleMapController mapController;
+  final LatLng _center = const LatLng(37.58638333, 127.0203333);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Route Page'),
+      ),
+      body: GoogleMap(
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(
+          target: _center,
+          zoom: 15.0,
+        ),
+        markers: {
+          Marker(
+            markerId: MarkerId('marker_id'),
+            position: _center,
+            infoWindow: InfoWindow(
+              title: 'Sample Marker',
+              snippet: 'This is a sample marker on Google Maps',
+            ),
+          ),
+        },
+      ),
+    );
+  }
+}
