d2acbd76f6a621fd2cb4f857edebccb80a479999
Hyeon Jung Nam <namhj99@naver.com>
2023-07-17 16:30:26 +0900

    7/17 뒷부분 ui 만드는 중.. ~~아직 탭바밖에 못만들었다

--- lib/View/main_route_page.dart
+++ lib/View/main_route_page.dart
@@ -23,26 +23,59 @@ class _MainRoutePageState extends State<MainRoutePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Main Route Page'),
-      ),
-      body: GoogleMap(
-        onMapCreated: _onMapCreated,
-        initialCameraPosition: CameraPosition(
-          target: _center,
-          zoom: 15.0,
-        ),
-        markers: {
-          Marker(
-            markerId: MarkerId('marker_id'),
-            position: _center,
-            infoWindow: InfoWindow(
-              title: 'Sample Marker',
-              snippet: 'This is a sample marker on Google Maps',
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(kToolbarHeight),
+          child: Container(
+            color: Theme.of(context).primaryColor,
+            child: TabBar(
+              tabs: const [
+                Tab(text: 'AI추천'),
+                Tab(text: '둘러보기'),
+              ],
+              labelStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold), // Increase font size for active tab
+              unselectedLabelStyle: TextStyle(fontSize: 18), // Font size for inactive tabs
+              padding:EdgeInsets.only(top: MediaQuery.of(context).padding.top),
             ),
           ),
-        },
+        ),
+        body: TabBarView(
+          children: [
+            _buildAIRecommendationTab(),
+            _buildTourTab(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAIRecommendationTab() {
+    return GoogleMap(
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(
+        target: _center,
+        zoom: 15.0,
+      ),
+      markers: {
+        Marker(
+          markerId: MarkerId('marker_id'),
+          position: _center,
+          infoWindow: InfoWindow(
+            title: 'Sample Marker',
+            snippet: 'This is a sample marker on Google Maps',
+          ),
+        ),
+      },
+    );
+  }
+
+  Widget _buildTourTab() {
+    return Center(
+      child: Text(
+        'AI recommendation tab content',
+        style: TextStyle(fontSize: 20, color: Colors.black),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:nagaja_app/View/main_page.dart';
 import 'package:nagaja_app/View/main_page_loading.dart';
 
@@ -7,6 +8,14 @@ import 'package:nagaja_app/View/main_page_loading.dart';
 void main() async {
   await _initialize();
   runApp(MyApp());
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent, // Set status bar color to transparent
+    statusBarIconBrightness: Brightness.dark, // Set status bar icons to dark color
+  ));
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [
+    SystemUiOverlay.top, // Enable manual control of the status bar
+    SystemUiOverlay.bottom, // Enable manual control of the navigation bar (if present)
+  ]);
 }
 
 class MyApp extends StatelessWidget {
