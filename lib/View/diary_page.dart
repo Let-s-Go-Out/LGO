5e3581fa68168ef42ac6fed6f9f391f7f58b497e
igaeun <igaeun0112@gmail.com>
2023-07-22 04:43:25 +0900

    Diary Page - Calendar 적용

--- lib/View/diary_page.dart
+++ lib/View/diary_page.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class DiaryPage extends StatelessWidget {
-  const DiaryPage ({super.key});
+class DiaryPage extends StatefulWidget {
+  const DiaryPage({super.key});
+
+  @override
+  State<DiaryPage> createState() => _DiaryPageState();
+}
+
+class _DiaryPageState extends State<DiaryPage> {
+
+  DateTime selectedDay = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
+
+  DateTime focusedDay = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('This is Diary Page.'),
+    return Scaffold(
+      appBar: AppBar(),
+      body: TableCalendar(
+        firstDay: DateTime.utc(2021, 10, 16),
+        lastDay: DateTime.utc(2030, 3, 14),
+        focusedDay: focusedDay,
+        onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
+          // 선택된 날짜의 상태 갱신
+          setState((){
+            this.selectedDay = selectedDay;
+            this.focusedDay = focusedDay;
+          });
+        },
+        selectedDayPredicate: (DateTime day) {
+          // selectedDay 와 동일한 날짜의 모양 바꿔줌
+          return isSameDay(selectedDay, day);
+        },
+      ),
     );
   }
 }
+
