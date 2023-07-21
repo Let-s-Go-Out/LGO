4089b883274012442769f469e5965807acf62f76
igaeun <igaeun0112@gmail.com>
2023-07-22 05:42:57 +0900

    Diary Page - Event marker 구현

--- lib/View/diary_page.dart
+++ lib/View/diary_page.dart
@@ -18,6 +18,15 @@ class _DiaryPageState extends State<DiaryPage> {
 
   DateTime focusedDay = DateTime.now();
 
+  Map<DateTime, List<Event>> events = {
+    DateTime.utc(2023,7,10) : [ Event('route1')],
+    DateTime.utc(2023,7,11) : [ Event('route2')],
+  };
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return events[day] ?? [];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,8 +46,23 @@ class _DiaryPageState extends State<DiaryPage> {
           // selectedDay 와 동일한 날짜의 모양 바꿔줌
           return isSameDay(selectedDay, day);
         },
+        calendarStyle: CalendarStyle(
+          markerSize: 36.9,
+          markersAnchor: 1.02,
+          markerDecoration: BoxDecoration(
+            color: Color.fromRGBO(255, 75, 75, 0.5),
+            shape: BoxShape.circle
+          ),
+        ),
+        eventLoader: _getEventsForDay,
       ),
     );
   }
+}
+
+class Event {
+  String title;
+
+  Event(this.title);
 }
 
