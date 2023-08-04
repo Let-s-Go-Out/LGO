94e2b02b4c27afa77c4889cf4a43bc504d9f881e
Hyeon Jung Nam <namhj99@naver.com>
2023-08-04 09:27:44 +0900

    8/4 코드 깔끔하게 수정

--- lib/View/main_route_page.dart
+++ lib/View/main_route_page.dart
@@ -1,9 +1,6 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
 import 'package:nagaja_app/Controller/map_controller.dart';
 import 'package:snapping_sheet_2/snapping_sheet.dart';
 
@@ -18,18 +15,21 @@ class MainRoutePage extends StatefulWidget {
 
 class _MainRoutePageState extends State<MainRoutePage> {
   late GoogleMapController mapController;
-  late GoogleMapController mapController2;
   // final LatLng _center = const LatLng(37.58638333, 127.0203333);
   MapController controller = MapController();
   bool isExpanded = false;
   ScrollController scrollcontroller = ScrollController();
-  Location location = Location();
   List<Place> places = [];
-  LocationData? locationData;
+  LatLng nowP = LatLng(37.58638333, 127.0203333);
 
   @override
   void initState() {
     super.initState();
+    controller.getPosition().then((position) {
+      setState(() {
+        controller.model.nowPosition = position;
+      });
+    });
   }
 
   @override
@@ -41,33 +41,10 @@ class _MainRoutePageState extends State<MainRoutePage> {
     mapController = controller;
   }
 
-  void _onMapCreated2(GoogleMapController controller) {
-    mapController2 = controller;
-  }
-
   Future<LatLng> _getInitialCameraPosition() async {
-    return await controller.getPosition().then((position) {
-      return LatLng(position.latitude, position.longitude);
-    });
+    nowP = LatLng(controller.model.nowPosition!.latitude, controller.model.nowPosition!.longitude);
+    return nowP;
   }
-
-  Future<LatLng> _getInitialCameraPosition2() async {
-    try {
-      locationData = await location.getLocation();
-      // await PlacesApi.searchPlaces(locationData!.latitude!, locationData!.longitude!);
-      //print (locationData.toString());
-      return LatLng(locationData!.latitude!, locationData!.longitude!);
-    } catch (e) {
-      print('Error getting location: $e');
-      return LatLng(37.58638333, 127.0203333); // Provide a fallback location if an error occurs
-    }
-  }
-
-  // Future<void> _updateCameraPosition(dynamic latlng) async {
-  //   await mapController.animateCamera(CameraUpdate.newLatLng(
-  //     LatLng(latlng.latitude, latlng.longitude),
-  //   ));
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -207,76 +184,76 @@ class _MainRoutePageState extends State<MainRoutePage> {
   }
 
   Widget _buildTourTab() {
-            return FutureBuilder<List<Place>>(
-                future: PlacesApi.searchPlaces(37.58638333, 127.0203333),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text('Error: ${snapshot.error}'),
-                    );
-                  } else {
-                    List<Place> places = snapshot.data ?? [];
-                    return Scaffold(
-                      body: SnappingSheet(
-                        lockOverflowDrag: true,
-                        snappingPositions: [
-                          SnappingPosition.factor(
-                              positionFactor: 0,
-                              grabbingContentOffset: GrabbingContentOffset.top),
-                          SnappingPosition.factor(
-                              positionFactor: 0.5,
-                              snappingCurve: Curves.elasticOut,
-                              snappingDuration: Duration(milliseconds: 1750)),
-                          SnappingPosition.factor(
-                            positionFactor: 0.9,
-                          )
-                        ],
-                        initialSnappingPosition:
-                            SnappingPosition.factor(positionFactor: 0.5),
-                        child: _buildTourTabContent(),
-                        grabbingHeight: 50,
-                        grabbing: GrabbingWidget(),
-                        sheetBelow: SnappingSheetContent(
-                          draggable: (details) => true,
-                          child: Container(
-                            color: Colors.white,
-                            child: ListView.builder(
-                              itemCount: places.length,
-                              itemBuilder: (context, index) {
-                                return PlaceCard(place: places[index]);
-                              },
-                            ),
-                          ),
-                        ),
-                      ),
-                      bottomNavigationBar: BottomNavigationBar(
-                        items: const <BottomNavigationBarItem>[
-                          BottomNavigationBarItem(
-                            icon: Icon(Icons.home),
-                            label: 'Home',
-                          ),
-                          BottomNavigationBarItem(
-                            icon: Icon(Icons.book),
-                            label: 'Diary',
-                          ),
-                          BottomNavigationBarItem(
-                            icon: Icon(Icons.person),
-                            label: 'Mypage',
-                          ),
-                        ],
-                      ),
-                    );
-                  }
-                });
+    return FutureBuilder<List<Place>>(
+        future: PlacesApi.searchPlaces(nowP.latitude, nowP.longitude),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Text('Error: ${snapshot.error}'),
+            );
+          } else {
+            List<Place> places = snapshot.data ?? [];
+            return Scaffold(
+              body: SnappingSheet(
+                lockOverflowDrag: true,
+                snappingPositions: [
+                  SnappingPosition.factor(
+                      positionFactor: 0,
+                      grabbingContentOffset: GrabbingContentOffset.top),
+                  SnappingPosition.factor(
+                      positionFactor: 0.5,
+                      snappingCurve: Curves.elasticOut,
+                      snappingDuration: Duration(milliseconds: 1750)),
+                  SnappingPosition.factor(
+                    positionFactor: 0.9,
+                  )
+                ],
+                initialSnappingPosition:
+                    SnappingPosition.factor(positionFactor: 0.5),
+                child: _buildTourTabContent(),
+                grabbingHeight: 50,
+                grabbing: GrabbingWidget(),
+                sheetBelow: SnappingSheetContent(
+                  draggable: (details) => true,
+                  child: Container(
+                    color: Colors.white,
+                    child: ListView.builder(
+                      itemCount: places.length,
+                      itemBuilder: (context, index) {
+                        return PlaceCard(place: places[index]);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              bottomNavigationBar: BottomNavigationBar(
+                items: const <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home),
+                    label: 'Home',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.book),
+                    label: 'Diary',
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    label: 'Mypage',
+                  ),
+                ],
+              ),
+            );
+          }
+        });
   }
 
   Widget _buildTourTabContent() {
     return FutureBuilder<LatLng>(
-      future: _getInitialCameraPosition2(),
+      future: _getInitialCameraPosition(),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(
@@ -290,7 +267,7 @@ class _MainRoutePageState extends State<MainRoutePage> {
           return GoogleMap(
             myLocationEnabled: true,
             myLocationButtonEnabled: false,
-            onMapCreated: _onMapCreated2,
+            onMapCreated: _onMapCreated,
             initialCameraPosition: CameraPosition(
               target: snapshot.data!,
               zoom: 15.0,
