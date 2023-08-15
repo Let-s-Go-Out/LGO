5156b1f762f8fad7d666da39f77f67ef4887ecd8
Hyeon Jung Nam <37244525+salmon99@users.noreply.github.com>
2023-08-15 18:08:24 +0900

    8/15 추천 장소 알고리즘 추가

--- lib/Model/draw_recommend_route.dart
+++ lib/Model/draw_recommend_route.dart
@@ -13,40 +13,47 @@ import 'package:nagaja_app/Model/place_model.dart';
 //DrawRecommendRoute routeDraw = DrawRecommendRoute(categoryGroupPlaceLists);
 class DrawRecommendRoute {
   static const String _apiKey = ' ';
+  List<String> categoryRestaurant = ['restaurant'];
+  List<String> categoryCafe = ['cafe'];
+  List<String> categoryShopping = ['store'];
+  List<String> categoryCulture = ['museum', 'library'];
+  List<String> categoryBar = ['bar'];
+  List<String> categoryAttraction = ['tourist_attraction', 'amusement_park', 'bowling_alley'];
 
   //수정 >> 사용자 설정 현재 위치, 데이터 받아 오기
   LatLng origin = LatLng(0.0, 0.0);
 
   //예시 >> 초기 리스트 비어 있음
-  List<Place> recommendPlaces = [
-    Place(
-    name: '스타벅스 성신여대점',
-    placeId: 'A',
-    placeLat: 37.591054,
-    placeLng: 127.022626,
-    types: ['cafe'],
-    rating: 0,
-    photoUrls: [''],
-  ),
-    Place(
-    name: '이디야 카페 성신여대점',
-    placeId: 'B',
-    placeLat: 37.591776,
-    placeLng: 127.021206,
-    types: ['cafe'],
-      rating: 0,
-    photoUrls: [''],
-  ),
-    Place(
-    name: '메가커피 성신여대점',
-    placeId: 'C',
-    placeLat: 37.590641,
-    placeLng: 127.021988,
-    types: ['cafe'],
-      rating: 0,
-    photoUrls: [''],
-  ),
-  ];
+  // List<Place> recommendPlaces = [
+  //   Place(
+  //   name: '스타벅스 성신여대점',
+  //   placeId: 'A',
+  //   placeLat: 37.591054,
+  //   placeLng: 127.022626,
+  //   types: ['cafe'],
+  //   rating: 0,
+  //   photoUrls: [''],
+  // ),
+  //   Place(
+  //   name: '이디야 카페 성신여대점',
+  //   placeId: 'B',
+  //   placeLat: 37.591776,
+  //   placeLng: 127.021206,
+  //   types: ['cafe'],
+  //     rating: 0,
+  //   photoUrls: [''],
+  // ),
+  //   Place(
+  //   name: '메가커피 성신여대점',
+  //   placeId: 'C',
+  //   placeLat: 37.590641,
+  //   placeLng: 127.021988,
+  //   types: ['cafe'],
+  //     rating: 0,
+  //   photoUrls: [''],
+  // ),
+  // ];
+  List<Place> recommendPlaces = [];
 
   Map<int, Polyline> polylineList = {};
   PolylinePoints polylinePoints = PolylinePoints();
@@ -55,11 +62,13 @@ class DrawRecommendRoute {
 
   //수정 >> 사용자 설정 추천 장소 갯수, 데이터 받아 오기
   int placesCounter = 3;
+  List<String> userType = ['음식점','문화'];
+
 
   //
-  DrawRecommendRoute(Map<String, List<Place>> categoryGroupPlaceLists) {
-    setRecommendPlaces(categoryGroupPlaceLists);
-  }
+  // DrawRecommendRoute(Map<String, List<Place>> categoryGroupPlaceLists) {
+  //   setRecommendPlaces(categoryGroupPlaceLists);
+  // }
 
   setOriginPlace(LatLng origin) {
     recommendPlaces.insert(0, Place(
@@ -75,63 +84,96 @@ class DrawRecommendRoute {
 
 
   //categoryGroupPlaceLists['category'], 예시 참고
-  setRecommendPlaces(Map<String, List<Place>> categoryGroupPlaceLists) {
-    setOriginPlace(DrawRecommendRoute(categoryGroupPlaceLists).origin);
-
-
-    for(var type in categoryGroupPlaceLists.keys) {
-      Map<String, int> typeList = {
-        '음식점': 1,
-        '카페': 1,
-        '쇼핑': 1,
-        '문화': 2,
-        '바': 1,
-        '어트랙션': 3,
-      };
-
-      //사용자가 선택한 type 인지 확인 후
-      //수정
-      var selectedType = {};
-
-      int addedPlaceCounter = 0;
-
-      // i < selectedType.length;
-      //categoryGroupPlaceLists[selectedType[i++]] => categoryGroupPlaceLists['음식점']
-      // => types: 'restaurant'
-
-      //categoryGroupPlaceLists[selectedType[i++]]
-      // => categoryGroupPlaceLists['어트랙션']
-      // => types: 'tourist_attraction', 'amusement_park', 'bowling_alley'
-      //
-
+  List<Place> setRecommendPlaces(Map<String, List<Place>> categoryGroupPlaceLists) {
+    setOriginPlace(LatLng(0, 0));
+    int a=0;
+    int b=0;
+    int c=0;
+    int d=0;
+    int e=0;
+    int f=0;
+    int culture = 0;
+    int attraction = 0;
+    List<Place>? cultureList =[];
+    List<Place>? attractionList = [];
+    for(int i=0;i<=placesCounter;) {
+      for (var type in userType) {
+        switch (type) {
+          case '음식점':
+            if(categoryGroupPlaceLists['음식점'] != null){
+              if(i<=placesCounter){
+                recommendPlaces.add(categoryGroupPlaceLists['음식점']![a]);
+                i++;
+                a++;
+              }
+            }
+            break;
+          case '카페':
+            if(categoryGroupPlaceLists['카페'] != null){
+              if(i<=placesCounter&&categoryGroupPlaceLists['카페']?.length != 0){
+                recommendPlaces.add(categoryGroupPlaceLists['카페']![b]);
+                i++;
+                b++;
+              }
+            }
+            break;
+          case '쇼핑':
+            if(categoryGroupPlaceLists['쇼핑'] != null){
+              if(i<=placesCounter&&categoryGroupPlaceLists['쇼핑']?.length != 0){
+                recommendPlaces.add(categoryGroupPlaceLists['쇼핑']![c]);
+                i++;
+                c++;
+              }
+            }
+            break;
+          case '문화':
+            if(categoryGroupPlaceLists['문화'] != null){
+              if(i<=placesCounter){
+                    cultureList = categoryGroupPlaceLists['문화']?.where((item) => categoryCulture.contains(item.types[culture%2])).toList();
+                    if(cultureList?.length != 0) {
+                      recommendPlaces.add(cultureList![d]);
+                    }
+                i++;
+                d++;
+                culture++;
+              }
+            }
+            break;
+          case '바':
+            if(categoryGroupPlaceLists['바'] != null){
+              if(i<=placesCounter&&categoryGroupPlaceLists['바']?.length != 0){
+                recommendPlaces.add(categoryGroupPlaceLists['바']![e]);
+                i++;
+                e++;
+              }
+            }
+            break;
+          case '어트랙션':
+            if(categoryGroupPlaceLists['어트랙션'] != null){
+              if(i<=placesCounter){
+                attractionList = categoryGroupPlaceLists['어트랙션']?.where((item) => categoryAttraction.contains(item.types[attraction%3])).toList();
+                if(attractionList?.length != 0){
+                  recommendPlaces.add(attractionList![f]);
+                }
+                i++;
+                f++;
+                attraction++;
+              }
+            }
+            break;
+        }
       }
     }
-
-
-    /*
-    //이미 별점 순서로 정렬
-    categoryGroupPlaceLists.forEach((place) {
-      // if(types 가 한 가지로만 이루어진 경우)
-      //recommendPlaces.addAll(categoryGroupPlaceLists.take(placesCounter));
-      if(!addedType.contains(place.types)) {
-        recommendPlaces.add(place);
-        addedPlaceCounter++;
-        addedType.add(place.types);
-      }
-      // if(recommendPlaces 에 types 가 한번씩 다 들어간 경우)
-      // addedType 리셋
-
-      if (addedPlaceCounter > placesCounter) { return ;}
-
-    });
-
-     */
-
-    sortingCloseDistance();
+    sortingCloseDistance(recommendPlaces);
+    print('추천장소');
+    for(int i =0;i<placesCounter+1;i++){
+      print(recommendPlaces[i].name);
+    }
+    return recommendPlaces;
   }
 
 
-  sortingCloseDistance() {
+  sortingCloseDistance(List<Place> recommendPlaces) {
     recommendPlaces.sort((a,b) {
       return a.distanceFromOrigin.compareTo(b.distanceFromOrigin);
     });
